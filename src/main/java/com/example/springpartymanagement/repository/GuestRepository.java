bbd0c032039ac1678db54f0a81d1813a6867c22d
Kibalo Jules Tinaka <julestnk.dev@gmail.com>
2023-02-09 02:25:06 +0100

    Add rest repository annotation on GuestRepository Add H2 database link in home template

--- src/main/java/com/example/springpartymanagement/repository/GuestRepository.java
+++ src/main/java/com/example/springpartymanagement/repository/GuestRepository.java
@@ -2,8 +2,10 @@ package com.example.springpartymanagement.repository;
 
 import com.example.springpartymanagement.entity.Guest;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@RepositoryRestResource(collectionResourceRel = "guest", path = "guest-repos-api")
 public interface GuestRepository extends JpaRepository<Guest, String> {
 }
